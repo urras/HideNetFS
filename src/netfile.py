df01a84d28d03f6ed4a0329a10a1c8fa9f7b9266
Zack <zack.mullaly@gmail.com>
2013-06-20 17:19:16 -0230

    Added file container

---
+++ src/netfile.py
@@ -0,0 +1,20 @@
+class NetFile(object):
+    '''A file that will be stored on the network.'''
+
+    def __init__(self, fid, partid, ptr):
+        self._file_id = fid
+        self._part_id = partid
+        self._path = ptr
+
+    def get_file_id(self):
+        return self._file_id
+
+    def get_part_id(self):
+        return self._part_id
+
+    def get_path(self):
+        return self._path
+
+    file_id = property(get_file_id, doc='Identification code of the file')
+    part_id = property(get_part_id, doc='Identification code of the file part')
+    path = property(get_path, doc='Path to the file in local storage')
