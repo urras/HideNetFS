dccb308215aea6acaee93cfc2312433c060b9e08
Zack <zack.mullaly@gmail.com>
2013-06-29 22:16:26 -0230

    Function for generating random file IDs

--- src/util.c
+++ src/util.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "util.h"
 
@@ -21,4 +22,21 @@ char* rstrstr(const char* haystack, const char* needle)
         pt1 = strstr(pt1, needle);
     } while (pt1 != NULL);
     return pt2;
+}
+
+char* randstr(char* dest, size_t length)
+{
+    int index = 0;
+    int r;
+
+    while (index < length) {
+        r = rand() % 122;
+        if ((r >= 'a' && r <= 'z') || 
+            (r >= 'A' && r <= 'Z') || 
+            (r >= '0' && r <= '9')) {
+            dest[index] = r;
+            index++;
+        }
+    }
+    return dest;
 }
--- src/util.h
+++ src/util.h
@@ -3,5 +3,6 @@
 
 size_t fcontent_length(FILE* f);
 char* rstrstr(const char* haystack, const char* needle);
+char* randstr(char* dest, size_t length);
 
 #endif /* __UTIL_H__ */
